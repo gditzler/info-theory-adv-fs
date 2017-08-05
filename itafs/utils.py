db6d24f3fd83c0eca0687bfeadd8c7db5045be9e
Gregory Ditzler <gregory.ditzler@gmail.com>
2017-08-05 00:05:47 -0700

    added a fcn for reading in generic files

--- itafs/utils.py
+++ itafs/utils.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python 
 
 import numpy as np 
+import pandas as pd
 
-def discretize_data(data, nbins=10):
+def discretize_data(data, n_bins=10):
   """Discretize a data set uniformly with as specified number of bins.
   Args:
     data (2D numpy array): Data set with n_samples \x n_features 
-    nbins (int): Number of bins to discretize the data 
+    n_bins (int): Number of bins to discretize the data 
 
   Returns:
     data (2D numpy array): Discretize data set with n_samples \x n_features
@@ -14,8 +15,26 @@ def discretize_data(data, nbins=10):
   data_out = data.copy()
   n_features = len(data_out[1])
   for n in range(n_features):
-    bins = np.linspace(np.min(data[:, n]), np.max(data[:,n]), nbins)
+    bins = np.linspace(np.min(data[:, n]), np.max(data[:,n]), n_bins)
     data_out[:,n]= np.digitize(data[:, n], bins)
   return data_out
 
+def read_file(file_name, sep=',', n_bins=None):
+  """Read in a delimited file. 
+  Args:
+    file_name (string): Data set location 
+    sep (string): file separator between columns (optional)
+    n_bins (int): number of bins for the data (optional)
 
+  Returns:
+    X (2D numpy array): Data set with n_samples \x n_features
+    y (numpy array): Vector of class labels
+  """
+  data = pd.read_csv(file_name, sep=sep, header=header)
+  X = data[:,:-1]
+  y = data[:,-1]
+
+  if n_bins is not None: 
+    X = discretize_data(X, n_bins)
+
+  return X, y
